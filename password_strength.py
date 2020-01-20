c9e63d6721ae148e2948e4769ede1229e49b4cf5
Gorbunov Pavel <pachagorbunoff@yandex.ru>
2020-01-20 23:16:11 +0300

    done password checking

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,28 @@
+import re
 
 
 def get_password_strength(password):
-    pass
+    strength = 0
+
+    if re.search(r'[a-z]', password):
+        strength += 1
+    
+    if re.search(r'[A-Z]', password):
+        strength += 1
+    
+    if re.search(r'[0-9]', password):
+        strength += 1
+    
+    #match special characters
+    if re.search(r'[^a-zA-Z0-9]', password):
+        strength += 1
+    
+    strength += len(password) // 3 % 6
+
+    return strength
+
 
 
 if __name__ == '__main__':
-    pass
+    user_password = input('Enter your password >')
+    print("The strength is", get_password_strength(user_password))
