cbac5d502c5061f2704dd07fde1af066e41526c7
Gorbunov Pavel <pachagorbunoff@yandex.ru>
2020-01-26 22:53:34 +0300

    add getpass add two rules

--- password_strength.py
+++ password_strength.py
@@ -1,27 +1,82 @@
 import re
+import getpass
+
+
+def get_point_on_lower_case(password):
+    if re.search(r'[a-z]', password):
+        return 1
+    return 0
+
+
+def get_point_on_upper_case(password):
+    if re.search(r'[A-Z]', password):
+        return 1
+    return 0
+
+
+def get_point_on_numbers(password):
+    if re.search(r'[0-9]', password):
+        return 1
+    return 0
+
+
+def get_point_on_special_symbols(password):
+    if re.search(r'[^a-zA-Z0-9]', password):
+        return 1
+    return 0
+
+
+def get_point_on_len_password(password):
+    return len(password) // 3 % 6
+
+
+def get_point_on_no_date_format(password):
+    if not re.search(r'(19\d{2})|(20\d{2})', password):
+        return 1
+    return 0
+
+
+def get_point_password_notin_blacklist(password):
+    blacklist = [
+        '123456',
+        'password',
+        '12345678,'
+        'qwerty',
+        '123456789',
+        '12345',
+        '1234',
+        '111111',
+        '1234567',
+        'dragon',
+        '123123',
+        'baseball',
+        'abc123',
+        'football',
+        'monkey',
+        'letmein',
+        '696969',
+        'shadow',
+        'master',
+        '666666',
+    ]
+    for word in blacklist:
+        if word in password:
+            return 0
+    return 1
 
 
 def get_password_strength(password):
     strength = 0
-
-    if re.search(r'[a-z]', password):
-        strength += 1
-
-    if re.search(r'[A-Z]', password):
-        strength += 1
-
-    if re.search(r'[0-9]', password):
-        strength += 1
-
-    # match special characters
-    if re.search(r'[^a-zA-Z0-9]', password):
-        strength += 1
-
-    strength += len(password) // 3 % 6
-
+    strength += get_point_on_lower_case(password)
+    strength += get_point_on_upper_case(password)
+    strength += get_point_on_numbers(password)
+    strength += get_point_on_special_symbols(password)
+    strength += get_point_on_len_password(password)
+    strength += get_point_on_no_date_format(password)
+    strength += get_point_password_notin_blacklist(password)
     return strength
 
 
 if __name__ == '__main__':
-    user_password = input('Enter your password >')
-    print("The strength is", get_password_strength(user_password))
+    user_password = getpass.getpass('Enter your password >')
+    print('The strength is', get_password_strength(user_password))
